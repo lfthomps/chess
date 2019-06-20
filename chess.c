9312dcb768a1e1a585fd0700f1024cb8e3c93a9c
Lowell Thompson <lowell.f.thompson@gmail.com>
2019-06-19 18:56:59 -0700

    added debug user input

--- chess.c
+++ chess.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 #include "display.h"
 #include "chessboard_api.h"
 
@@ -45,8 +46,67 @@ int main(int argc, char* argv[])
   assert(!chessboard_move(cb, G8, F6));
   assert(!chessboard_move(cb, E5, G4));
   assert(chessboard_move(cb, G1, H3));
+
   display_draw_chessboard(buffer, cb);
   printf("\n%s\n", buffer);
+
+  // This is obviously not permanent code - I just want to be able
+  // to test things a little more easily.  
+  while (true)
+  {
+      chessboard_square from = CHESSBOARD_MAX_SQUARE;
+      chessboard_square to = CHESSBOARD_MAX_SQUARE;
+      char input_buffer[4] = {};
+      unsigned char rank = '\0';
+      unsigned char file = '\0';
+      bool valid_square = true;
+      
+      printf("Enter from square: ");
+      if (input_buffer[0] == 'q')
+      {
+	  printf("\n");
+	  break;
+      }
+      fgets(input_buffer, 4, stdin);
+      file = tolower(input_buffer[0]);
+      rank = input_buffer[1];
+      if (file >= 'a' && file <= 'h' && rank >= '1' && rank <= '8')
+      {
+	  from = (7 - (int)(rank - '1')) * 8 + (int)(file - 'a');
+      }
+      else
+      {
+	  valid_square = false;
+      }
+      printf("Enter to square: ");
+      if (input_buffer[0] == 'q')
+      {
+	  printf("\n");
+	  break;
+      }
+      fgets(input_buffer, 4, stdin);
+            file = tolower(input_buffer[0]);
+      rank = input_buffer[1];
+      if (file >= 'a' && file <= 'h' && rank >= '1' && rank <= '8')
+      {
+	  to = (7 - (int)(rank - '1')) * 8 + (int)(file - 'a');
+      }
+      else
+      {
+	  valid_square = false;
+      }
+
+      if (valid_square && chessboard_move(cb, from, to))
+      {
+	  display_draw_chessboard(buffer, cb);
+	  printf("\n%s\n", buffer);
+      }
+      else
+      {
+	  printf("Illegal move\n");
+      }
+      
+  }
 
   chessboard_free(cb);
   free(buffer);
